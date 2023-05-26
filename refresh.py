b48a9124dab2b9471a457de339c98f2a21b8be67
Piyush Acharya <acharyapiyush1@outlook.com>
2023-05-26 16:06:51 +0000

    Mine Microsoft Rewards

---
+++ refresh.py
@@ -0,0 +1,12 @@
+#! /usr/bin/env python3
+import webbrowser
+from time import sleep
+
+URL = "https://www.bing.com/search?DAF0=1&FORM=ANNTA1&PC=U531&adlt=strict&aqs=edge.0.0l9.2078j0j1&cvid=3be7defb486d479eb5a9ee0cbb1d1311&pglt=161&q=hello" # your url
+COUNT = 0 # counter for refreshes
+
+while COUNT < 20: # loop until 4000 refreshes
+    webbrowser.open(URL) # open the url
+    sleep(0) # wait for 1 seconds
+    COUNT += 1 # increment the counter
+    URL += "hfhfhf"
