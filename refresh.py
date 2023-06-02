3226380497888d4bc79f639bf8a9581748f290e1
Piyush Acharya <acharyapiyush1@outlook.com>
2023-06-02 05:16:39 +0000

    Lower spam count

--- refresh.py
+++ refresh.py
@@ -5,7 +5,7 @@ from time import sleep
 URL = "https://www.bing.com/search?DAF0=1&FORM=ANNTA1&PC=U531&adlt=strict&aqs=edge.0.0l9.2078j0j1&cvid=3be7defb486d479eb5a9ee0cbb1d1311&pglt=161&q=hello" # your url
 COUNT = 0 # counter for refreshes
 
-while COUNT < 20: # loop until 4000 refreshes
+while COUNT < 1000: # loop until 4000 refreshes
     webbrowser.open(URL) # open the url
     sleep(0) # wait for 1 seconds
     COUNT += 1 # increment the counter
